d6503cea439be5c2549ce268d70e35cf2c4bf970
Jesus Jimenez <jesjimenez@gmail.com>
2015-11-18 16:49:25 +0100

    Initial import

---
+++ scripts/exportjson.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Extract Gourmet recipes from SQLite DB to JSON format
+# Dependencies: csvkit (csvjson command)
+
+# Path to Gourmet database
+RECIPESFILE="/home/u82066/.gourmet/recipes.db"
+
+echo "["
+recipes=()
+while read recipe; do
+    idrecipe=$(echo $recipe | cut -d"|" -f1)
+    titrecipe=$(echo $recipe | cut -d"|" -f2)
+    yields=$(echo $recipe | cut -d"|" -f3)
+    yieldunits=$(echo $recipe | cut -d"|" -f4)
+    instructions=$(echo $recipe | cut -d"|" -f5)
+
+    ingredients=$(sqlite3 -header -csv $RECIPESFILE "select amount,unit,item from ingredients where recipe_id=$idrecipe order by position" | csvjson)
+
+    if [[ $ingredients ]]; then
+        recipes+=("{\"id\":\"$idrecipe\",\"title\":\"$titrecipe\",\"yields\":\"$yields\",\"yieldunits\":\"$yieldunits\",\"ingredients\":$ingredients,\"instructions\":\"$instructions\"}")
+    fi
+done < <(sqlite3 $RECIPESFILE 'select id,title,yields,yield_unit,instructions from recipe;' | sed 's/^[0-9].*/INILIN\0/g' | tr '\n' '#' | sed 's/#INILIN/INILIN/g' | sed 's/INILIN/\nINILIN/g'| sed 's/^INILIN//g' | sed '/^$/d;s/#/<br>/g')
+printf -v recipelist ",%s" "${recipes[@]}"
+recipelist=${recipelist:1}
+echo $recipelist
+echo "]"
---
+++ web/code.js
@@ -0,0 +1,79 @@
+recipes={};
+currentrecipe=0;
+
+/* Fill ingredients with specified multiplier */
+function fill_ingredients(recipeid,multiplier) {
+    original_multiplier=recipes[recipeid].yields;
+    if (original_multiplier==0)
+        original_multiplier=1;
+
+    /* If yield number is undefined (0 value), don't scale anything */
+    mult=multiplier;
+    if (mult==0)
+        mult=1;
+
+    $("ul#ingredients").empty();
+    $.each(recipes[id].ingredients,function(key,val) {
+        finalamount=(val.amount/original_multiplier)*mult;
+        /* Format decimals if it's not a round number */
+        if ((finalamount % 1)!=0)
+            finalamount=finalamount.toFixed(2);
+        /* Don't show it if there's no quantity */
+        if (finalamount==0)
+            finalamount="";
+/*        if (finalamount!=Math.trunc(finalamount)) {
+            var f=new Fraction(finalamount-Math.trunc(finalamount));
+            finalamount=Math.trunc(finalamount) + " " + f.numerator + '/' + f.denominator;
+        }*/
+
+        $("ul#ingredients").append("<li>"+finalamount+" "+val.unit+" "+val.item+"</li>");
+    });
+}
+
+$( document ).ready(function() {
+    $.getJSON("recipes.json",function(data) {
+        /* Re-index data by id */
+        for(i=0;i<data.length;i++)
+            recipes[data[i].id]=data[i];
+
+        /* Populate list of recipes */
+        var items = [];
+        $.each( recipes, function( key, val ) {
+            items.push( "<li id='" + key + "'>" + val.title + "</li>" );
+        });
+
+        $( "<ul/>", {
+            "class": "recipelist",
+            html: items.join( "" )
+        }).appendTo( "div#recipelist" );
+
+        /* Activate recipe detail div when clicking on a recipe name */
+        $("li").click(function(eventObject) {
+            id=eventObject.target.id;
+            currentrecipe=id;
+
+            $("#yields").val(recipes[id].yields);
+            $("#yieldunits").text(recipes[id].yieldunits);
+            $("#instructions").html(recipes[id].instructions);
+
+            fill_ingredients(id,recipes[id].yields);
+
+            /* Switch visibility of divs, and change title */
+            $("#recipelist").hide(100);
+            $("#recipedetail").show(100,function(){$("#title").text(recipes[id].title);});
+        });
+    });
+
+    /* Go back to recipe list */
+    $("#backbutton").click(function() {
+        $("#recipedetail").hide(100);
+        $("#recipelist").show(100,function(){$("#title").text("Lista de recetas");});
+    });
+
+    /* Adjust ingredients when changing yields */
+    $("#yields").change(function(){
+        newmultiplier=$("#yields").val();
+        fill_ingredients(currentrecipe,newmultiplier);
+    });
+
+});
