cdc16bb6746c5114460ae2beb5246061ac5f2286
Jesus Jimenez <jesjimenez@gmail.com>
2015-11-24 13:11:13 +0100

    Extended script to obtain notes and category

--- scripts/exportjson.sh
+++ scripts/exportjson.sh
@@ -20,13 +20,15 @@ while read recipe || [[ -n $recipe ]]; do
     yields=$(echo $recipe | cut -d"|" -f3)
     yieldunits=$(echo $recipe | cut -d"|" -f4)
     instructions=$(echo $recipe | cut -d"|" -f5)
+    modifications=$(echo $recipe | cut -d"|" -f6)
+    category=$(echo $recipe | cut -d"|" -f7)
 
     ingredients=$(sqlite3 -header -csv $RECIPESFILE "select amount,unit,item from ingredients where recipe_id=$idrecipe order by position" | csvjson)
 
     if [[ $ingredients ]]; then
-        recipes+=("{\"id\":\"$idrecipe\",\"title\":\"$titrecipe\",\"yields\":\"$yields\",\"yieldunits\":\"$yieldunits\",\"ingredients\":$ingredients,\"instructions\":\"$instructions\"}")
+        recipes+=("{\"id\":\"$idrecipe\",\"title\":\"$titrecipe\",\"category\":\"$category\",\"yields\":\"$yields\",\"yieldunits\":\"$yieldunits\",\"ingredients\":$ingredients,\"instructions\":\"$instructions\",\"modifications\":\"$modifications\"}")
     fi
-done < <(sqlite3 $RECIPESFILE 'select id,title,yields,yield_unit,instructions from recipe;' | sed 's/^[0-9].*/INILIN\0/g' | tr '\n' '#' | sed 's/#INILIN/INILIN/g' | sed 's/INILIN/\nINILIN/g'| sed 's/^INILIN//g' | sed '/^$/d;s/#/<br>/g')
+done < <(sqlite3 $RECIPESFILE 'select r.id,r.title,r.yields,r.yield_unit,r.instructions,r.modifications,c.category from recipe r left outer join categories c on r.id=c.recipe_id;' | sed 's/^[0-9].*/INILIN\0/g' | tr '\n' '#' | sed 's/#INILIN/INILIN/g' | sed 's/INILIN/\nINILIN/g'| sed 's/^INILIN//g' | sed '/^$/d;s/#/<br>/g')
 printf -v recipelist ",%s" "${recipes[@]}"
 recipelist=${recipelist:1}
 echo $recipelist
